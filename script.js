f9d8776d5eccf6d458e230a60b67986ac152d142
Farman71 <farmanhussain71@gmail.com>
2025-01-04 14:12:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function shortcut(s1, s2) {
   // your code here
+	let result = "";
+
+  // Check if s1 is not empty, then add the first character
+  if (s1.length > 0) {
+    result += s1[0];
+  }
+
+  // Check if s2 is not empty, then add the first character
+  if (s2.length > 0) {
+    result += s2[0];
+  }
+
+  // Return the result
+  return result;
 }
 
 // Do not change the code below.
